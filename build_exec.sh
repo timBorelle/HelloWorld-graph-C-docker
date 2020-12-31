87acfef96a4a7bb7c61c93889b35ad4127c61004
timBorelle <tim-borelle@protonmail.com>
2020-12-31 17:55:03 +0100

    update build_exec

--- build_exec.sh
+++ build_exec.sh
@@ -3,10 +3,8 @@
 # Create Docker image
 docker build -t hello_world-graph-c-docker .
 
-# Launch the container and execute
-docker run -it \
-	-e DISPLAY=:0 \
-	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	--rm --name HelloWorld-graph-container \
-	hello_world-graph-c-docker
-
+# Launch the container and execute (read only)
+docker run --rm -it \
+	-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+	-v ~/.Xauthority:/home/user/.Xauthority:ro \
+	-e DISPLAY hello_world-graph-c-docker
